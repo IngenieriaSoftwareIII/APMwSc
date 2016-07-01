132aeed3a486fa9e542f79ec2b5375d9f804c63c
Jonnathan Ng <ngjonnathan@gmail.com>
2016-07-01 11:23:51 -0400

    Agrupando pruebas

--- testing/testSuiteWithoutEclipse.py
+++ testing/testSuiteWithoutEclipse.py
@@ -1,38 +1,31 @@
 # -*- coding: utf-8 -*-.
 
-import unittest
 import os
+import unittest
 
 os.chdir("testing")
 
+#from testAcceptanceCriteria     import *
 from testAccions                import *
 from testActorsUserHistory      import *
 from testArchivo                import *
 from testBackLog                import *
 from testCategory               import *
-from testHistory                import *
 from testElementMeeting			import *
+from testHistory                import *
 from testLogin                  import *
+from testMeeting 				import *
 from testObjective              import *
 from testObjectivesUserHistory  import *
-from testMeeting 				import *
 from testPrecedence             import *
 from testPrueba                 import *
-from testElementMeeting			import *
-from testHistory                import *
-from testLogin                  import *
-from testMeeting 				import *
-from testObjective              import *
-from testObjectivesUserHistory  import *
-#from testPrueba                 import *
 from testRole                   import *
 from testSprint                 import *
+#from testsubEquipoClass         import *
 from testTask                   import *
 from testTeam                   import *
 from testUser                   import *
 from testUserHistory            import *
-#from testArchivo                import *
-#from testPrueba                 import *
 
 if __name__ == '__main__':
     unittest.main()
